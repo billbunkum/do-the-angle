9dd4f407559039748ef9e90d1b997a34029f72d8
billbunkum <william.richard.hurst@gmail.com>
2016-10-06 16:19:07 -0400

    at least angular is working?

--- app.js
+++ app.js
@@ -1,6 +1,28 @@
 angular.module('app', [])
     .controller('MainCtrl', mainController);
 
-function mainController ($scope){
+function mainController() {
+    const ctrl = this;
+    ctrl.user_input = {};
 
-}
+//this houses 'task objects'
+    ctrl.tasks = [
+        {
+            name: "garbage",
+//            state: false,
+//            timestamp: Date.now();
+        },
+        {
+            name: "dinner",
+//            state: false,
+//            timestamp: Date.now();
+        }
+    ];
+
+    function addTask() {
+        ctrl.tasks.push(ctrl.user_input);
+        ctrl.user_input = {};
+    };
+
+
+} //end of mainController
